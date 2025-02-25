d28cb5cb599974d9c0e4e42d0c59afc7db9e1dc9
yeji <49176310+Yeggggi@users.noreply.github.com>
2025-02-26 00:09:23 +0900

    [FEAT/#18] AlarmSettingView의SearchButtonView, 알림시작 버튼과 하단 Scroll 버튼 UI

--- HotCha/HotCha/Views/AlarmSettingView/AlarmSettingView.swift
+++ HotCha/HotCha/Views/AlarmSettingView/AlarmSettingView.swift
@@ -31,14 +31,14 @@ struct ModalView: View{
                 Spacer()
                 
                 SearchButtonView()
-                //                StatusView()
+//                AlarmStatusView()
             }
         }
         .edgesIgnoringSafeArea(.all)
     }
 }
 
-struct StatusView: View {
+struct AlarmStatusView: View {
     var body: some View {
         VStack {
             Text("간선 1234")
@@ -83,40 +83,66 @@ struct SearchButtonView: View {
             
             Divider()
             
-            ZStack(alignment: .center){
-                HStack() {
-                    Text("1/2")
-                        .foregroundStyle(.gray300)
-                    Spacer()
-                    Text("정류장 선택")
-                        .foregroundStyle(.mainpurple)
-                }
-                .padding(EdgeInsets(top: 15, leading: 20, bottom: 37, trailing: 20))
-                
-                HStack(alignment:.center, spacing: 0){
-                        Ellipse()
-                            .frame(width: 32, height: 32)
-                            .foregroundStyle(.gray200)
-                            .overlay(
-                                Image("bt_up")
-                            )
-                            .padding(.trailing, 20)
-                        
-                        Ellipse()
-                            .frame(width: 32, height: 32)
-                            .foregroundStyle(.mainpurple)
-                            .overlay(
-                                Image("bt_down")
-                            )
-                    }
-                .padding(EdgeInsets(top: 15, leading: 0, bottom: 37, trailing: 0))
-                }
-            
-            
+//            AlarmSearchScrollButtonView()
+            MainPurpleAlarmButton()
             
         }
     }
 }
+
+struct AlarmSearchScrollButtonView: View {
+    var body: some View {
+        ZStack(alignment: .center){
+            HStack() {
+                Text("1/2")
+                    .foregroundStyle(.gray300)
+                Spacer()
+                Text("정류장 선택")
+                    .foregroundStyle(.mainpurple)
+            }
+            .padding(EdgeInsets(top: 15, leading: 20, bottom: 37, trailing: 20))
+            
+            HStack(alignment:.center, spacing: 0){
+                Ellipse()
+                    .frame(width: 32, height: 32)
+                    .foregroundStyle(.gray200)
+                    .overlay(
+                        Image("bt_up")
+                    )
+                    .padding(.trailing, 20)
+                
+                Ellipse()
+                    .frame(width: 32, height: 32)
+                    .foregroundStyle(.mainpurple)
+                    .overlay(
+                        Image("bt_down")
+                    )
+            }
+            .padding(EdgeInsets(top: 15, leading: 0, bottom: 37, trailing: 0))
+        }
+    }
+}
+
+
+struct MainPurpleAlarmButton: View {
+    var body: some View {
+        HStack(alignment: .center){
+            Spacer()
+            Text("알림 시작")
+                .font(.pretendard(.semibold, size: 20))
+                .foregroundStyle(.gray50)
+                .padding(.vertical, 12)
+                .frame(maxWidth: .infinity)
+                .background(
+                    RoundedRectangle(cornerRadius: 8).fill(.mainpurple)
+                        .frame(maxWidth: .infinity)
+                )
+                .padding(EdgeInsets(top: 16, leading: 20, bottom: 36, trailing: 20))
+            Spacer()
+        }
+    }
+}
+
 
 #Preview {
     AlarmSettingView()
