94bfb50209212692d1c4869a4a3d66234dc9831e
tenedict <tenedict@gmail.com>
2025-03-11 16:30:05 +0900

    [Feat/#27] Navigation  Indicator

--- HotCha/HotCha/Views/MainView/NavigationView.swift
+++ HotCha/HotCha/Views/MainView/NavigationView.swift
@@ -9,52 +9,87 @@ import SwiftUI
 
 struct NavigationView: View {
     @State var selectedTab = 0
+    @State private var isTouching = true
 
     
     var body: some View {
-               VStack {
+               VStack(spacing: 0) {
                    HStack {
                        // 위치 정보
                        MyLocationView()
                        Spacer()
                        
+                       ZStack {
+                           // 홈 버튼
+                           Text("홈")
+                               .padding(.horizontal,10)
+                               .font(selectedTab == 0 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
+                               .foregroundColor(selectedTab == 0 ? Color("mainpurple") : Color("gray600"))
+                               .onTapGesture {
+                                   withAnimation {
+                                       selectedTab = 0
+                                   }
+                               }
+                           
+                               VStack(spacing: 0) {
+                                   Spacer()
+                                   Image("NavigationPoint")
+                                       .offset(y: selectedTab == 0 ? 30 : 40)
+                                       .opacity(selectedTab == 0 ? 1 : 0)
+                                       .animation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 0.1), value: selectedTab)
+                               }
+                           
+                       }
                        
-                       // 홈 버튼
-                       Text("홈")
-                           .padding(.horizontal,10)
-                           .font(selectedTab == 0 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
-                           .foregroundColor(selectedTab == 0 ? Color("mainpurple") : Color("gray600"))
-                           .onTapGesture {
-                               withAnimation {
-                                   selectedTab = 0
+                       ZStack {
+                           // 이용기록 버튼
+                           Text("이용 기록")
+                               .padding(.horizontal,10)
+                               .font(selectedTab == 1 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
+                               .foregroundColor(selectedTab == 1 ? Color("mainpurple") : Color("gray600"))
+                               .onTapGesture {
+                                   withAnimation {
+                                       selectedTab = 1
+                                   }
+                               }
+                       
+                               VStack(spacing: 0) {
+                                   Spacer()
+                                   Image("NavigationPoint")
+                                       .offset(y: selectedTab == 1 ? 30 : 40)
+                                       .opacity(selectedTab == 1 ? 1 : 0)
+                                       .animation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 0.1), value: selectedTab)
+                               }
+                           
+
+                       }
+                       ZStack {
+                           // 설정 버튼
+                           Text("설정")
+                               .padding(.horizontal,10)
+                               .padding(.trailing, 10)
+                               .font(selectedTab == 2 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
+                               .foregroundColor(selectedTab == 2 ? Color("mainpurple") : Color("gray600"))
+                               .onTapGesture {
+                                   withAnimation {
+                                       selectedTab = 2
+                                   }
+                               }
+                           if isTouching {
+                               VStack(spacing: 0) {
+                                   Spacer()
+                                   Image("NavigationPoint")
+                                       .offset(y: selectedTab == 2 ? 30 : 40)
+                                       .opacity(selectedTab == 2 ? 1 : 0)
+                                       .animation(.spring(response: 0.3, dampingFraction: 1, blendDuration: 0.1), value: selectedTab)
                                }
                            }
-                       
-                       // 이용기록 버튼
-                       Text("이용 기록")
-                           .padding(.horizontal,10)
-                           .font(selectedTab == 1 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
-                           .foregroundColor(selectedTab == 1 ? Color("mainpurple") : Color("gray600"))
-                           .onTapGesture {
-                               withAnimation {
-                                   selectedTab = 1
-                               }
-                           }
-                       
-                       // 설정 버튼
-                       Text("설정")
-                           .padding(.horizontal,10)
-                           .padding(.trailing, 10)
-                           .font(selectedTab == 2 ? .pretendard(.bold, size: 24) : .pretendard(.regular, size: 24))
-                           .foregroundColor(selectedTab == 2 ? Color("mainpurple") : Color("gray600"))
-                           .onTapGesture {
-                               withAnimation {
-                                   selectedTab = 2
-                               }
-                           }
+                           
+
+                       }
                    }
-                   .frame(height: 62)
-                   .padding(.top, 54)
+                       .frame(height: 62)
+                       .padding(.top, 54)
                    
 
                    // 아래쪽 탭뷰
@@ -77,6 +112,7 @@ struct NavigationView: View {
                        }
                    // 인디케이터 숨겼음, 페이지 탭뷰로 스와이프 기능 넣음
                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                   
                }.background(Color("gray900")) // 전채 배경색
             // 아래쪽 여백 제거
             .ignoresSafeArea(.all)       
