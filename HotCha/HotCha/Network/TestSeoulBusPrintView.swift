9a7f9744440e6e3ba27655676d154e04fdfaa0f3
yeji <49176310+Yeggggi@users.noreply.github.com>
2025-04-01 14:51:23 +0900

    [FEAT/#43] 버스 정류장 리스트 호출

--- HotCha/HotCha/Network/TestSeoulBusPrintView.swift
+++ HotCha/HotCha/Network/TestSeoulBusPrintView.swift
@@ -13,41 +13,43 @@ struct TestSeoulBusPrint : View{
     @State private var routeNo: String = ""
     
     var body: some View{
-        VStack{
-            Button (action: {
-                fetchSeoulBusAPI(citycode: 1) { fetchedBuses in
-                    // API 호출 후 데이터 받아오면 로딩 상태 해제
-                    self.allBuses = fetchedBuses.sorted(using: KeyPathComparator(\.routeno))
-                    self.filteredBuses = fetchedBuses.sorted(using: KeyPathComparator(\.routeno))
+        NavigationStack {
+            VStack{
+                Button (action: {
+                    fetchSeoulBusAPI(citycode: 1) { fetchedBuses in
+                        // API 호출 후 데이터 받아오면 로딩 상태 해제
+                        self.allBuses = fetchedBuses.sorted(using: KeyPathComparator(\.routeno))
+                        self.filteredBuses = fetchedBuses.sorted(using: KeyPathComparator(\.routeno))
+                    }
+                    
+                }){
+                    Text("Click Me")
                 }
-                
-            }){
-                Text("Click Me")
-            }
-            ScrollView(showsIndicators: false) {
-                
-                    ForEach(filteredBuses) { bus in
-                        
-                        // 네비게이션 링크: 선택된 버스가 있을 때 SelectBusStopView로 이동
-                        NavigationLink(destination: SelectBusStopView(bus: bus, cityCode: 1)){
-                        VStack(alignment: .leading) {
-                            Spacer()
-                            VStack(alignment: .leading) {
-                                Text("\(bus.routeno)")
-                                    .font(.body)
-                                    .foregroundStyle(.black)
-                                    .padding(.bottom, 4)
-                                HStack {
-                                    Text("\(bus.startnodenm) - \(bus.endnodenm)")
-                                        .font(.caption2)
-                                        .foregroundStyle(.black)
-                                }
-                            }
-                            .padding(.horizontal, 16)
+                ScrollView(showsIndicators: false) {
+                    
+                        ForEach(filteredBuses) { bus in
                             
-                            Divider()
-                                .padding(.top, 20)
-                                .padding(.bottom, 16)
+                            // 네비게이션 링크: 선택된 버스가 있을 때 SelectBusStopView로 이동
+                            NavigationLink(destination: SelectBusStopView(bus: bus, cityCode: 1)){
+                            VStack(alignment: .leading) {
+                                Spacer()
+                                VStack(alignment: .leading) {
+                                    Text("\(bus.routeno)")
+                                        .font(.body)
+                                        .foregroundStyle(.black)
+                                        .padding(.bottom, 4)
+                                    HStack {
+                                        Text("\(bus.startnodenm) - \(bus.endnodenm)")
+                                            .font(.caption2)
+                                            .foregroundStyle(.black)
+                                    }
+                                }
+                                .padding(.horizontal, 16)
+                                
+                                Divider()
+                                    .padding(.top, 20)
+                                    .padding(.bottom, 16)
+                            }
                         }
                     }
                 }
