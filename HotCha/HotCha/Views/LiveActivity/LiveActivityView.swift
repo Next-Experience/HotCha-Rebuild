e2099abacb988ef5b14a162b858ed34b145989a8
Ho Mun <mhhh2082@gmail.com>
2025-04-09 00:22:52 +0900

    [Feat/#56] Fork Test

---
+++ HotCha/HotCha/Views/LiveActivity/LiveActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  LiveActivityView.swift
+//  HotCha
+//
+//  Created by 문호 on 4/9/25.
+//
+
+import SwiftUI
+
+struct LiveActivityView: View {
+    var body: some View {
+        Text("뉴준성은 신이다")
+    }
+}
+
+#Preview {
+    LiveActivityView()
+}
