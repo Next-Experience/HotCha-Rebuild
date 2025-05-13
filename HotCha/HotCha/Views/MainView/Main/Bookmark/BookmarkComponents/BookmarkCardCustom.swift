f5284c03a3b34b88f055d61ad089a305e690daeb
yeji <49176310+Yeggggi@users.noreply.github.com>
2025-05-13 19:53:34 +0900

    [FEAT/#124] 북마크 카드에서 AlarmSettingView로 navigate + navigate하기 위해 필요한 ViewModel, sheetManager 전달

--- HotCha/HotCha/Views/MainView/Main/Bookmark/BookmarkComponents/BookmarkCardCustom.swift
+++ HotCha/HotCha/Views/MainView/Main/Bookmark/BookmarkComponents/BookmarkCardCustom.swift
@@ -15,6 +15,15 @@ struct BookmarkCardCustomView: View {
     @State private var isTapped: Bool = false
     @State private var showingAlert = false
     @Environment(\.modelContext) private var modelContext
+    @State var isBookmark: Bool = false // 필요 x, but navigate 에 필요
+    @Binding var type_name: String // 필요 x, but navigate 에 필요
+    
+    @ObservedObject var modalStateViewModel: AlarmModalViewModel
+    @ObservedObject var busStopSeoulViewModel: BusStopSeoulViewModel
+    @ObservedObject var nearestBusViewModel: NearestBusViewModel
+    @ObservedObject var sheetManager: AlarmSettingModalSheetManager
+    
+    @State private var shouldNavigate = false  // 네비게이션 트리거
     
     private func deleteBookmark(_ bookmark: Bookmarkmodel) {
         modelContext.delete(bookmark)
@@ -25,9 +34,9 @@ struct BookmarkCardCustomView: View {
         }
     }
     
-//
+    //
     
-
+    
     
     var body: some View {
         VStack {
@@ -64,7 +73,7 @@ struct BookmarkCardCustomView: View {
                 
                 VStack(spacing: 6) {
                     HStack {
-//                        BookmarkBusNoView(busNo: bookmark.bus_no, route_type: bookmark.route_type)
+                        //                        BookmarkBusNoView(busNo: bookmark.bus_no, route_type: bookmark.route_type)
                         SearchBusUtil.CustomBusNoView(busNo: bookmark.bus_no, routeType: bookmark.route_type)
                         Spacer()
                         
@@ -84,28 +93,51 @@ struct BookmarkCardCustomView: View {
         .background(isTapped ? Color("gray300") : Color("gray150"))
         .cornerRadius(8)
         .onTapGesture {
-            if isEditMode == false {
-                if alarmActive {
-                    showingAlert = true
-                } else {
-                    withAnimation {
-                        isTapped = true
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        withAnimation {
-                            isTapped = false
+                    if isEditMode == false {
+                        if alarmActive {
+                            showingAlert = true
+                        } else {
+                            withAnimation {
+                                isTapped = true
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                withAnimation {
+                                    isTapped = false
+                                }
+                            }
+                            
+                            // 알람 시작
+                            print("이부분 실행돼?")
+                            busStopSeoulViewModel.shortcutDestinationId = bookmark.destination_stop_id
+                            busStopSeoulViewModel.isReload = true
+                            modalStateViewModel.modalState = .alertStopsMedium
+                            busStopSeoulViewModel.shortcutExecute = true // 알람 실행에 필요한 데이터를 로드하기 위한  트리거
+                            shouldNavigate = true  // 네비게이션 트리거
+                            print("여기는 실행돼?")
+                            
+                           
                         }
                     }
                 }
-            }
-        }
+                .navigationDestination(isPresented: $shouldNavigate) {  // .constant 대신 $shouldNavigate 사용
+                    AlarmSettingView(
+                        bus: .constant(bookmark.bus),
+                        cityCode: .constant(1),
+                        isBookmark: $isBookmark,
+                        type_name: $type_name,
+                        modalStateViewModel: modalStateViewModel,
+                        busStopSeoulViewModel: busStopSeoulViewModel,
+                        nearestBusViewModel: nearestBusViewModel,
+                        sheetManager: sheetManager
+                    )
+                }
         .alert("새로운 알림을 시작하시겠어요?", isPresented: $showingAlert) {
             Button("그만두기", role: .cancel) {
                 // 취소 동작
             }
             Button("실행하기") {
                 // 기존 알림 취소 후 새 알림 설정
-
+                
             }
         } message: {
             Text("알림은 한 개만 설정할 수 있어요. 새로운 알림을 시작하면 기존의 설정한 알림이 취소돼요.")
